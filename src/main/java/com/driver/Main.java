aa1def21dad3a4f537bde309ed16a28f264acd4c
sahilkumar013 <sahilkashyap7056@gmail.com>
2023-01-16 19:52:20 +0530

    encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
+import java.sql.SQLOutput;
 public class Main {
+    private static void main(String[] args){
+        RWOnly obj = new RWOnly();
+        obj.setName("Sahil");
+        System.out.println("Error:you might seen some error,please not it down in comments");
+        obj.getName();
+    }
   
 }
